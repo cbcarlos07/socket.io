32ef16a9bfc1ccc4ebed9f056b5f1c946870a679
Carlos Bruno <cbcarlos08@gmail.com>
2019-06-04 07:30:56 -0400

    -

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+
+const io = require('socket.io')(http)
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
+io.on('connection', (socket)=>{
+    //console.log('new socket',socket)
+    console.log('new socket',socket.id)
+})
+
+http.listen(3000, ()=>{
+    console.log('Ouvindo na porta 3000')
+})
